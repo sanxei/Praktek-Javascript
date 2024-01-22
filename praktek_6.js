94200c4faa35ec13f75bb336183e95a3ec9c1bc2
sanxei <sandeki.2796@gmail.com>
2024-01-22 13:28:04 +0800

    aoy

---
+++ praktek_6.js
@@ -0,0 +1,34 @@
+// let number = 10 + " 10"
+// console.log(number)
+
+// let str = "42"
+// let num = ++str
+// console.log(num)
+
+
+
+// const birthYear = prompt("Tahun Berapa Kamu Lahir ? ")
+// const  urAge = 2024 - (birthYear) //mengubah string menjadi number
+// console.log(urAge)
+
+// let num = 42
+// let str = String(num) //mengubah number menjadi string
+// console.log(str)
+
+
+const age = 20
+const height = 167
+const weight = 60
+const isStudent = true
+
+const ageAsString = String(age)
+const heightAsString = height + "cm"
+const weightAsString = weight + "kg"
+const studentStatus = Number(isStudent)
+
+console.log(typeof ageAsString)
+console.log(typeof heightAsString)
+console.log(typeof weightAsString)
+console.log(typeof studentStatus)
+
+

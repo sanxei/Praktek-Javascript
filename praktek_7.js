bc7c976caff08aca69b252e1697046f3540b3d7f
sanxei <sandeki.2796@gmail.com>
2024-01-22 13:52:58 +0800

    aoy

---
+++ praktek_7.js
@@ -0,0 +1,44 @@
+// let pancasila = 2
+
+// switch (pancasila) {
+//     case 1:
+//         console.log("Ketuhanan yang maha esa")
+//         break;
+//     case 2:
+//         console.log("Kemanusiaan yang adil dan beradab")
+//         break;
+//     case 3:
+//         console.log("Persatuan indonesia")
+//         break;
+//     case 4:
+//         console.log("Kerakyatan yang dipimpin oleh hikmat kebijaksanaan dalam permusywaratan perwakailan")
+//         break;
+//     case 5:
+//         console.log("Keadilan sosial bagi seluruh rakyat indonesia")
+//     default:
+//         console.log("Nilai tidak valid")
+//         break;
+// }
+
+let plat = "KT"
+
+switch (plat) {
+    case "KU":
+        console.log("Plat Nomor Polisi Daerah Kalimantan Utara")
+        break;
+    case "KT":
+        console.log("Plat Nomor Polisi Daerah Kalimantan Timur")
+        break;
+    case "DA":
+        console.log("Plat Nomor Polisi Daerah Kalimantan Selatan")
+        break;
+    case "KB":
+        console.log("Plat Nomor Polisi Daerah Kalimantan Barat")
+        break;
+    case "KH":
+        console.log("Plat Nomor Polisi Daerah Kalimantan Tengah")
+        break;
+    default:
+        console.log("Nilai tidak valid")
+        break;
+}
